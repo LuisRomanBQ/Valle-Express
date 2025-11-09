12fec0d1dd516a276e1e05264cfb16ed748163ec
LuisRomanBQ <romanborbon111@gmail.com>
2025-11-08 18:36:30 -0700

    icon2

--- Valle Express/AppShell.xaml.cs
+++ Valle Express/AppShell.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace Valle_Express
+﻿using Valle_Express.Views;
+
+namespace Valle_Express
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(ClienteMainView), typeof(ClienteMainView));
+            Routing.RegisterRoute(nameof(RepartidorMainView), typeof(RepartidorMainView));
+            Routing.RegisterRoute(nameof(LoginView), typeof(LoginView));
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new LoginView());
         }
     }
 }
